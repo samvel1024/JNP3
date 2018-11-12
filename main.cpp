d73c3845fc39a5b8a9e08b42212792fc969b750f
Samvel Abrahamyan <samvel1024@gmail.com>
2018-11-12 16:11:15 +0100

    Initial definitions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+
+
+
+//Sandbox test
+
+class MyClass {
+    int val;
+
+
+public:
+    MyClass(int val) : val(val) {
+    }
+
+    MyClass() {
+
+    }
+
+    MyClass &operator=(const MyClass &rhs) {
+
+        // Only do assignment if RHS is a different object from this.
+
+        if (this != &rhs) {
+            this->val = rhs.val;
+        }
+
+        return *this;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    MyClass a1(1);
+    MyClass a3(3);
+     a3 = a1;
 }
