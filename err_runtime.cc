a3cbdcb72012d2e75ff70eb61016b5982cfcab10
Jacek <jac.karwowski@gmail.com>
2018-11-20 13:43:27 +0100

    Runtime errors check

---
+++ err_runtime.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "wallet.h"
+
+/**
+ * For asserting runtime failures
+ * Change -DERR1 to desired test case
+ * g++-8 -Wall -Wextra -O2 -std=c++17 -DERR1 wallet.h err_test.cc -o wallet.o
+ */
+
+int main() {
+#ifdef ERR1
+    Wallet w1(-5);
+#endif
+#ifdef ERR2
+    Wallet w1(3);
+    w1 -= 4;
+#endif
+#ifdef ERR3
+    Wallet w1(3);
+    w1 += (-4);
+#endif
+#ifdef ERR4
+    Wallet w1(3), w2(4);
+    w1 -= w2;
+#endif
+#ifdef ERR5
+    Wallet w1;
+    w1 = 1 - 2;
+#endif
+#ifdef ERR6
+    Wallet w1;
+    w1 *= -1;
+#endif
+#ifdef ERR7
+    Wallet w1;
+    w1 = Wallet(1) - Wallet(2);
+#endif
+    return 0;
+}
